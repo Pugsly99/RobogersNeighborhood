97d7f0fe69ab12b3300da0be6736cfe580e93c91
Pugsly99 <rylandadams@yahoo.com>
2020-10-30 10:55:42 -0700

    .includes to the if statements to check multiple digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ function robotSwitch(num) {
   const numArray = [];
 
   for (let i = 0; i <= num; i++) {
-    if (i === 3) {
+    let check = i.toString();
+
+    if (check.includes(3)) {
       numArray.push("Won't you be my neighbor?" + ", ")
-    } else if (i === 2){
+    } else if (check.includes(2)){
       numArray.push("Boop!" + ", ")
-    } else if (i === 1){
+    } else if (check.includes(1)){
       numArray.push("Beep!" + ", ")
     } else{
       numArray.push(i + ", ")
